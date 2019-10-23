4e211a39ee3e552e94369ac760edaa080ef602c1
sbentley2019 <sbentley000@gmail.com>
2019-10-23 22:53:27 +0000

    Logged the error in the 'else' statement.

--- fetcher.js
+++ fetcher.js
@@ -5,16 +5,16 @@ const fetcher = function(args) {
   const fs = require('fs');
 
   request(args[0], (error, response, body) => {
-    // console.log('error:', error);
     if (response && response.statusCode === 200) {
       fs.writeFile(args[1], body, (err) => {
         if (err) throw err;
         console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
       });
     } else {
-      console.log("bad url was input :(")
+      console.log("bad url was input :(");
+      console.log('error:', error);
     }
   });
 };
 
-fetcher(args)
+fetcher(args);
